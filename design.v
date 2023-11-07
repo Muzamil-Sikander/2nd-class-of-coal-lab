d2ae8fa82226a9a19fedd91a9a2c9eae60da562e
muzamilsikander <102538533+Muzamil-Sikander@users.noreply.github.com>
2023-11-07 12:57:21 +0500

    Add files via upload

---
+++ design.v
@@ -0,0 +1,24 @@
+//  Combinational Circuit
+module C_Circuit (a,b,c,o);
+
+//  declaring inputs and output
+ input a;
+ input b;
+ input c;
+
+ output o;
+
+//  Interm Wire
+
+wire X ;
+
+assign x = a | b ;
+assign o = x & c ;
+
+
+
+
+
+
+endmodule
+
